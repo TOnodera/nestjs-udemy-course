21292910601f768097fd82f6e9dcc031dd86b708
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-04-26 20:16:58 +0900

    ログインまで

---
+++ flea-market/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from 'src/entities/user.entity';
+import { CredentialsDto } from './dto/credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('signup')
+    async sighUp(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return await this.authService.signUp(createUserDto);
+    }
+
+    @Post('signin')
+    async signIn(@Body() creadentialsDto: CredentialsDto): Promise<{ accessToken: string }> {
+        return await this.authService.signIn(creadentialsDto);
+    }
+}
